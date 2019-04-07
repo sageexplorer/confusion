5079942f622b458a4e7551e479f7b8de1be3e21f
sageexplorer <srimal@campusexplorer.com>
2019-04-06 19:10:15 -0700

    add Provider for the redux store

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import Main from './components/MainComponent'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
 import "./App.css";
 
 class App extends Component {
+  
 
   render() {
+    const store = ConfigureStore();
     return (
+      <Provider store={store}>
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+      </BrowserRouter>
+      </Provider>
     );
   }
 }
